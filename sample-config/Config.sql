d93ff0734860c85e5a0a66662bafd2fc3a7c9a02
Pavol Malo <pavol.malo@ucloudify.com>
2025-08-10 20:35:51 +0200

    docs config: add sample

---
+++ sample-config/Config.sql
@@ -0,0 +1,59 @@
+﻿CREATE TABLE [ctl].[Config] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [Name]        VARCHAR (100) NULL,
+    [Value]       VARCHAR (100) NULL,
+    [Scope]       VARCHAR (100) NULL,
+    [Description] VARCHAR (100) NULL,
+    CONSTRAINT [PK_Config] UNIQUE NONCLUSTERED ([Name] ASC, [Scope] ASC)
+);
+
+
+GO
+
+CREATE TRIGGER [ctl].[trg_UPDATE_Config] ON [ctl].[Config]
+AFTER UPDATE
+AS
+BEGIN
+    INSERT INTO [ctl].[ConfigAudit] ([Id],[Name],[Value],[Scope],[Description],[ChangeType],[ChangedBy],[ChangeDate])
+    SELECT i.[Id]
+        ,i.[Name]
+        ,i.[Value]
+        ,i.[Scope]
+        ,i.[Description]
+        ,'UPDATE'
+        ,SUSER_SNAME()
+        ,GETDATE()
+    FROM INSERTED i;
+END;
+GO
+CREATE TRIGGER [ctl].[trg_INSERT_Config] ON [ctl].[Config]
+AFTER INSERT
+AS
+BEGIN
+    INSERT INTO [ctl].[ConfigAudit] ([Id],[Name],[Value],[Scope],[Description],[ChangeType],[ChangedBy],[ChangeDate])
+    SELECT i.[Id]
+        ,i.[Name]
+        ,i.[Value]
+        ,i.[Scope]
+        ,i.[Description]
+        ,'INSERT'
+        ,SUSER_SNAME()
+        ,GETDATE()
+    FROM INSERTED i;
+END;
+GO
+CREATE TRIGGER [ctl].[trg_DELETE_Config] ON [ctl].[Config]
+AFTER DELETE
+AS
+BEGIN
+    INSERT INTO [ctl].[ConfigAudit] ([Id],[Name],[Value],[Scope],[Description],[ChangeType],[ChangedBy],[ChangeDate])
+    SELECT i.[Id]
+        ,i.[Name]
+        ,i.[Value]
+        ,i.[Scope]
+        ,i.[Description]
+        ,'DELETE'
+        ,SUSER_SNAME()
+        ,GETDATE()
+    FROM DELETED i;
+END;
---
+++ sample-config/ConfigAudit.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [ctl].[ConfigAudit] (
+    [Id]          INT            NOT NULL,
+    [Name]        VARCHAR (100)  NULL,
+    [Value]       VARCHAR (100)  NULL,
+    [Scope]       VARCHAR (100)  NULL,
+    [Description] VARCHAR (100)  NULL,
+    [ChangeType]  NVARCHAR (10)  NULL,
+    [ChangedBy]   NVARCHAR (128) NULL,
+    [ChangeDate]  DATETIME       NULL
+);
+
