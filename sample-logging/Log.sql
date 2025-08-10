e3722fd73f59a8794b874d93e6f784d6fe356184
Pavol Malo <pavol.malo@ucloudify.com>
2025-08-10 20:36:01 +0200

    docs logging: add sample

---
+++ sample-logging/Log.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [ctl].[Log] (
+    [Id]        BIGINT        IDENTITY (1, 1) NOT NULL,
+    [Timestamp] DATETIME      DEFAULT (sysdatetime()) NOT NULL,
+    [Source]    VARCHAR (256) NOT NULL,
+    [BatchId]   BIGINT        NULL,
+    [User]      VARCHAR (128) DEFAULT (suser_sname()) NULL,
+    [Status]    VARCHAR (10)  NULL,
+    [Level]     VARCHAR (10)  NOT NULL,
+    [Message]   VARCHAR (MAX) NULL,
+    CONSTRAINT [CHK_LogLevel] CHECK ([Level]='TRACE' OR [Level]='DEBUG' OR [Level]='INFO' OR [Level]='WARNING' OR [Level]='ERROR' OR [Level]='FATAL'),
+    CONSTRAINT [CHK_Stage] CHECK ([Status]='STARTED' OR [Status]='PROCESSING' OR [Status]='FINISHED' OR [Status]='FAILED' OR [Status]='CANCELLED' OR [Status]=NULL)
+);
+
---
+++ sample-logging/spLogINFO.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE [ctl].[spLogINFO]
+( 
+	@BatchId INT,
+    @Timestamp DATETIME = NULL,
+	@Source VARCHAR(256),
+    @User VARCHAR(128),
+	@Status VARCHAR(10),
+    @LogLevelConfig INT = 4, -- default log level configuration (0=trace, 1=debug, 2=info, 3=warning, 4=error, 5=fatal)
+	@Message VARCHAR(max)
+)
+AS
+BEGIN 
+    
+    IF @LogLevelConfig > 2
+        RETURN; -- do not log 
+    ELSE
+	    INSERT INTO [ctl].[Log] ([BatchId],[Timestamp],[Source],[User],[Status],[Level],[Message])
+	    VALUES (
+		    @BatchId,
+		    CASE WHEN @Timestamp IS NULL THEN GETDATE() ELSE @Timestamp END,
+		    @Source,
+            @User,
+		    @Status,
+		    'INFO',
+		    @Message
+		    );
+END
